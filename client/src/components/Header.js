d0ec163649d1098f4b0004daf4bde120c42449eb
GarenLiang <ideabreeze@gmail.com>
2017-08-07 20:38:06 +0800

    redirect

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return 'Still deciding';
+      case false:
+        return <li><a href="/auth/google">Login with Google</a></li>;
+      default:
+        return <li><a href="/api/logout">Logout</a></li>;
+    }
+  }
   render() {
     return (
       <nav>
@@ -9,14 +20,15 @@ class Header extends Component {
             Emaily
           </a>
           <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
     );
   }
 }
+function mapStateToProps({ auth }) {
+  return { auth };
+}
 
-export default Header;
+export default connect(mapStateToProps)(Header);
