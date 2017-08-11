16510b4a16ed0d5ffcec7f01cfb191bc9a600f83
GarenLiang <ideabreeze@gmail.com>
2017-08-11 20:54:47 +0800

    route in production

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 mongoose.connect(keys.mongoURI);
 
 const app = express();
